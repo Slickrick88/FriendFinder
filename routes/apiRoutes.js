bf82376fbabf8e0cea286233a585130378dceee3
Slickrick88 <rickablake@gmail.com>
2018-03-21 18:00:13 -0400

    update

---
+++ routes/apiRoutes.js
@@ -0,0 +1,23 @@
+
+var friendsData = require("../data/friends");
+
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        res.json(friendsData);
+    });
+
+    app.post("/api/friends", function (req, res) {
+        console.log("api post called");
+        friendsData.push(req.body);
+        res.json(true);
+
+    });
+
+
+    app.post("/api/clear", function () {
+        // Empty out the arrays of data
+        friendsData = [];
+        console.log(tableData);
+    });
+};
